e9e60defded0ff9dcfa0e0233848cbb45350f711
Agus Torres <torresagustin613@gmail.com>
2026-02-04 16:29:52 -0300

    feat(homebanking): add get and post methods to transfers endpoint

---
+++ src/models/transfers.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field, func
+from pydantic import BaseModel
+
+
+class Transfer(SQLModel, table=True):
+    __tablename__ = "transfers"
+
+    id: int = Field(default=None, primary_key=True)
+    sender_id: int = Field(foreign_key="users.id")
+    receiver_id: int = Field(foreign_key="users.id")
+    balance: float = Field(default=0.0)
+    transfer_date: datetime = Field(
+        sa_column_kwargs={"server_default": func.now()}, nullable=False
+    )
+
+
+class TransferCreate(BaseModel):
+    sender_id: int
+    receiver_id: int
+    account_number: str
+    balance: float
--- src/routers/api/itbank/homebanking/homebanking.py
+++ src/routers/api/itbank/homebanking/homebanking.py
@@ -1,10 +1,15 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
-from src.constants.constants import HOME_BANKING_PREFIX
+from src.constants.constants import (
+    HOME_BANKING_PREFIX,
+    ACCOUNTS_PREFIX,
+    TRANSFERS_PREFIX,
+)
 from src.routers.api.auth.utils import get_current_user
 from src.models.users import User
 from src.db.connection import SessionDep
 from src.models.accounts import BankAccount
+from src.models.transfers import Transfer, TransferCreate
 from sqlmodel import select
 
 
@@ -15,10 +20,13 @@ router = APIRouter(prefix=HOME_BANKING_PREFIX, dependencies=[Depends(get_current
 async def get_homebanking(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    return {"message": f"Welcome to ITBANK Homebanking {current_user.username.capitalize()}!"}
+    return {
+        "message": f"Welcome to ITBANK Homebanking {current_user.username.capitalize()}!"
+    }
 
 
-@router.get("/accounts")
+# ACCOUNTS
+@router.get(ACCOUNTS_PREFIX)
 async def get_my_accounts(
     session: SessionDep,
     current_user: Annotated[User, Depends(get_current_user)],
@@ -30,7 +38,7 @@ async def get_my_accounts(
     return accounts
 
 
-@router.get("/accounts/{account_number}")
+@router.get(f"{ACCOUNTS_PREFIX}/{{account_number}}")
 async def get_account_details(
     account_number: str,
     session: SessionDep,
@@ -52,3 +60,66 @@ async def get_account_details(
         )
 
     return account
+
+
+# TRANSFERS
+@router.get(f"{TRANSFERS_PREFIX}")
+async def get_transfer_history(
+    session: SessionDep,
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    transfers = session.exec(
+        select(Transfer).where(
+            Transfer.sender_id == current_user.id,
+            Transfer.receiver_id == current_user.id,
+        )
+    ).all()
+
+    return transfers
+
+
+@router.post(f"{TRANSFERS_PREFIX}")
+async def make_transfer(
+    transfer: TransferCreate,
+    session: SessionDep,
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    sender_account = session.exec(
+        select(BankAccount).where(BankAccount.user_id == current_user.id)
+    ).first()
+
+    receiver_account = session.exec(
+        select(BankAccount).where(
+            BankAccount.user_id == transfer.receiver_id
+            and BankAccount.account_number == transfer.account_number
+        )
+    ).first()
+
+    if not receiver_account:
+        raise HTTPException(status_code=404, detail="Receiver account not found")
+
+    if sender_account.balance < transfer.balance:
+        raise HTTPException(status_code=400, detail="Insufficient funds")
+
+    sender_account.balance -= transfer.balance
+    receiver_account.balance += transfer.balance
+
+    transfer = Transfer(
+        sender_id=current_user.id,
+        receiver_id=receiver_account.user_id,
+        balance=transfer.balance,
+    )
+
+    session.add(transfer)
+    session.add(sender_account)
+    session.add(receiver_account)
+    session.commit()
+    session.refresh(transfer)
+    session.refresh(sender_account)
+    session.refresh(receiver_account)
+
+    return {
+        "sender": sender_account,
+        "receiver": receiver_account,
+        "balance": transfer.balance,
+    }
