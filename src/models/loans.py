90add643217a40d14d1a335bc4a9ab3670cbd674
Agus Torres <torresagustin613@gmail.com>
2026-02-19 01:11:41 -0300

    feat(homebanking): add loan models and endpoints

---
+++ src/models/loans.py
@@ -0,0 +1,34 @@
+from sqlmodel import SQLModel, Field, func
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class Loan(SQLModel, table=True):
+    __tablename__ = "loans"
+
+    id: int = Field(default=None, primary_key=True)
+    branch_office_id: int = Field(foreign_key="branch_offices.id")
+    user_id: int = Field(foreign_key="users.id")
+    user_account_id: int = Field(foreign_key="bank_accounts.user_id")
+    loan_type: str
+    start_date: datetime = Field(
+        sa_column_kwargs={"server_default": func.now()}, nullable=False
+    )
+    due_date: float
+    amount: float
+    status: str
+    interest_rate: float
+    total_repayment: float
+    total_installments: int
+    remaining_installments: int
+
+
+class LoanCreate(BaseModel):
+    branch_office_id: int
+    user_account_id: int
+    loan_type: str
+    due_date: float
+    amount: float
+    interest_rate: float
+    total_installments: int
+    remaining_installments: int
--- src/routers/api/itbank/homebanking/homebanking.py
+++ src/routers/api/itbank/homebanking/homebanking.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from src.constants.constants import (
     HOME_BANKING_PREFIX,
     ACCOUNTS_PREFIX,
@@ -11,6 +11,7 @@ from src.db.connection import SessionDep
 from src.models.accounts import BankAccount
 from src.models.cards import Card
 from src.models.transfers import Transfer, TransferCreate
+from src.models.loans import Loan, LoanCreate
 from sqlmodel import select
 
 
@@ -142,3 +143,76 @@ async def get_card_list(
     ).all()
 
     return cards
+
+
+# LOANS
+@router.get("/loans")
+async def get_loan_list(
+    session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]
+):
+    loans = session.exec(select(Loan).where(Loan.user_id == current_user.id)).all()
+
+    return loans
+
+
+@router.get("/loans/{loan_id}")
+async def get_loan_by_id(
+    loan_id: int,
+    session: SessionDep,
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    loan = session.exec(
+        select(Loan).where(Loan.id == loan_id, Loan.user_id == current_user.id)
+    ).first()
+
+    if not loan:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No loan found with id {loan_id}",
+        )
+
+    return loan
+
+
+@router.post("/loans")
+async def request_loan(
+    loan: LoanCreate,
+    session: SessionDep,
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    user_account = session.exec(
+        select(BankAccount).where(
+            BankAccount.id == loan.user_account_id,
+            BankAccount.user_id == current_user.id,
+        )
+    ).first()
+
+    if not user_account:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Account not found"
+        )
+
+    total_to_pay = loan.amount * loan.interest_rate
+    new_loan = Loan(
+        user_id=current_user.id,
+        total_repayment=total_to_pay,
+        amount=loan.amount,
+        branch_office_id=loan.branch_office_id,
+        due_date=loan.due_date,
+        interest_rate=loan.interest_rate,
+        loan_type=loan.loan_type,
+        total_installments=loan.total_installments,
+        remaining_installments=loan.remaining_installments,
+        user_account_id=loan.user_account_id,
+        status="active",
+    )
+
+    user_account.balance += loan.amount
+
+    session.add(new_loan)
+    session.add(user_account)
+    session.commit()
+    session.refresh(new_loan)
+    session.refresh(user_account)
+
+    return {"account": user_account, "loan": new_loan}
