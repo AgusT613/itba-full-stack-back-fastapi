fab41f9f102fc7e74eb3a09cb1930242780c4f38
Agus Torres <torresagustin613@gmail.com>
2026-02-24 15:01:28 -0300

    test: add test cases for patch, put and delete http methods

--- tests/accounts/accounts_test.py
+++ tests/accounts/accounts_test.py
@@ -18,7 +18,9 @@ def test_get_my_accounts_no_accounts(client_auth):
 def test_get_account_details_not_found(client_auth):
     auth_client, _ = client_auth()
 
-    response = auth_client.get(f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/999999999")
+    response = auth_client.get(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/by/?acc_number=nonexistent&acc_id=9999"
+    )
 
     assert response.status_code == 404
     data = response.json()
@@ -84,7 +86,7 @@ def test_get_account_details_success(client_auth, session, fake):
     session.refresh(account)
 
     response = auth_client.get(
-        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/{account.account_number}"
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/by/?acc_number={account.account_number}&acc_id={account.id}"
     )
 
     assert response.status_code == 200
@@ -159,3 +161,187 @@ def test_create_account_for_user(client_auth, fake):
     assert data["account_number"] == new_account.account_number
     assert data["balance"] == new_account.balance
     assert data["description"] == new_account.description
+
+
+def test_partial_update_account(client_auth, session, fake):
+    """Test partially updating an account"""
+    auth_client, user = client_auth()
+
+    account = BankAccount(
+        account_number=fake.unique.random_number(digits=16, fix_len=True),
+        balance=3000.00,
+        user_id=user.id,
+        account_type=fake.random_element(elements=("checking", "savings")),
+        description=fake.sentence(),
+    )
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+
+    update_data = {
+        "account_number": account.account_number,
+        "account_type": "updated_type",
+    }
+
+    response = auth_client.patch(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}", json=update_data
+    )
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert data["account_type"] == update_data["account_type"]
+    assert data["balance"] == account.balance  # balance should not be updated
+    assert (
+        data["description"] == account.description
+    )  # description should not be updated
+
+
+def test_partial_update_account_not_found(client_auth):
+    """Test partially updating a non-existent account"""
+    auth_client, _ = client_auth()
+
+    update_data = {
+        "account_number": "nonexistent",
+        "account_type": "updated_type",
+    }
+
+    response = auth_client.patch(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}", json=update_data
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    data = response.json()
+    assert data["detail"] == "Account not found"
+
+
+def test_full_update_account(client_auth, session, fake):
+    """Test fully updating an account"""
+    auth_client, user = client_auth()
+
+    account = BankAccount(
+        account_number=fake.unique.random_number(digits=16, fix_len=True),
+        balance=3000.00,
+        user_id=user.id,
+        account_type=fake.random_element(elements=("checking", "savings")),
+        description=fake.sentence(),
+    )
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+
+    update_data = {
+        "account_number": account.account_number,
+        "account_type": "updated_type",
+        "balance": 5000.00,
+        "description": "Updated description",
+    }
+
+    response = auth_client.put(f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}", json=update_data)
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert data["account_type"] == update_data["account_type"]
+    assert data["balance"] == update_data["balance"]
+    assert data["description"] == update_data["description"]
+
+
+def test_full_update_account_not_found(client_auth):
+    """Test fully updating a non-existent account"""
+    auth_client, _ = client_auth()
+
+    update_data = {
+        "account_number": "nonexistent",
+        "account_type": "updated_type",
+        "balance": 5000.00,
+        "description": "Updated description",
+    }
+
+    response = auth_client.put(f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}", json=update_data)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    data = response.json()
+    assert data["detail"] == "Account not found"
+
+
+def test_delete_account_by_number(client_auth, session, fake):
+    """Test deleting an account by account number"""
+    auth_client, user = client_auth()
+
+    account = BankAccount(
+        account_number=fake.unique.random_number(digits=16, fix_len=True),
+        balance=3000.00,
+        user_id=user.id,
+        account_type=fake.random_element(elements=("checking", "savings")),
+        description=fake.sentence(),
+    )
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+
+    response = auth_client.delete(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/?acc_number={account.account_number}"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["detail"] == "Account deleted successfully"
+    assert data["account_deleted"]["account_number"] == account.account_number
+    assert data["account_deleted"]["id"] == account.id
+    assert data["account_deleted"]["user_id"] == user.id
+    assert data["account_deleted"]["account_type"] == account.account_type
+    assert data["account_deleted"]["balance"] == account.balance
+    assert data["account_deleted"]["description"] == account.description
+
+
+def test_delete_account_by_id(client_auth, session, fake):
+    """Test deleting an account by account ID"""
+    auth_client, user = client_auth()
+
+    account = BankAccount(
+        account_number=fake.unique.random_number(digits=16, fix_len=True),
+        balance=3000.00,
+        user_id=user.id,
+        account_type=fake.random_element(elements=("checking", "savings")),
+        description=fake.sentence(),
+    )
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+
+    response = auth_client.delete(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/?acc_id={account.id}"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["detail"] == "Account deleted successfully"
+    assert data["account_deleted"]["account_number"] == account.account_number
+    assert data["account_deleted"]["id"] == account.id
+    assert data["account_deleted"]["user_id"] == user.id
+    assert data["account_deleted"]["account_type"] == account.account_type
+    assert data["account_deleted"]["balance"] == account.balance
+    assert data["account_deleted"]["description"] == account.description
+
+
+def test_delete_account_wrong_parameters(client_auth):
+    """Test deleting an account with missing parameters"""
+    auth_client, _ = client_auth()
+
+    response = auth_client.delete(f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    data = response.json()
+    assert data["detail"] == "Either account_id or account_number must be provided"
+
+
+def test_delete_account_not_found(client_auth):
+    """Test deleting a non-existent account"""
+    auth_client, _ = client_auth()
+
+    response = auth_client.delete(
+        f"{ITBANK_ACCOUNTS_COMPLETE_ENDPOINT}/?acc_number=nonexistent&acc_id=9999"
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    data = response.json()
+    assert data["detail"] == "Account not found"
--- tests/homebanking_test.py
+++ tests/homebanking_test.py
@@ -13,3 +13,16 @@ def test_get_homebanking_welcome_message(client_auth):
     assert "cards" in data
     assert "transfers" in data
     assert data["user"]["username"] == user.username
+
+
+def test_get_homebanking_current_user_info(client_auth):
+    auth_client, user = client_auth()
+
+    response = auth_client.get(f"{ITBANK_HOMEBANKING_COMPLETE_ENDPOINT}/me")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert "email" in data
+    assert "full_name" in data
+    assert "username" in data
+    assert data["username"] == user.username
