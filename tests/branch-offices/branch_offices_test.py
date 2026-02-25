f152c432766d8abfb9d2e0303d38121630688b4b
Agus Torres <torresagustin613@gmail.com>
2026-02-25 13:08:30 -0300

    test(branch-office): add cases for post and delete http methods

--- tests/branch-offices/branch_offices_test.py
+++ tests/branch-offices/branch_offices_test.py
@@ -67,3 +67,49 @@ def test_branch_offices_response_structure(client, session, fake):
     assert "name" in branch_office_data
     assert "address" in branch_office_data
     assert "contact" in branch_office_data
+
+
+def test_create_branch_office(client, session, fake):
+    response = client.post(ITBANK_BRANCH_OFFICES_COMPLETE_ENDPOINT)
+    assert response.status_code == 200
+    data = response.json()
+    assert "id" in data
+    assert "name" in data
+    assert "address" in data
+    assert "contact" in data
+
+    branch_office_in_db = session.get(BranchOffice, data["id"])
+    assert branch_office_in_db is not None
+    assert branch_office_in_db.name == data["name"]
+    assert branch_office_in_db.address == data["address"]
+    assert branch_office_in_db.contact == data["contact"]
+
+
+def test_delete_branch_office(client, session, fake):
+    branch_office = BranchOffice(
+        name=fake.company(), address=fake.address(), contact=fake.phone_number()
+    )
+
+    session.add(branch_office)
+    session.commit()
+
+    response = client.delete(
+        f"{ITBANK_BRANCH_OFFICES_COMPLETE_ENDPOINT}/{branch_office.id}"
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "detail" in data
+    assert data["detail"] == "Branch office deleted successfully"
+    assert "deleted_branch_office" in data
+    assert data["deleted_branch_office"]["id"] == branch_office.id
+
+    branch_office_in_db = session.get(BranchOffice, branch_office.id)
+    assert branch_office_in_db is None
+
+
+def test_delete_nonexistent_branch_office(client):
+    response = client.delete(f"{ITBANK_BRANCH_OFFICES_COMPLETE_ENDPOINT}/9999")
+    assert response.status_code == 404
+    data = response.json()
+    assert "detail" in data
+    assert data["detail"] == "Branch office not found"
