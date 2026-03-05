3c94ac6416b11f67891060cc2db710652f3ba3d0
Agus Torres <torresagustin613@gmail.com>
2026-03-05 16:55:57 -0300

    test(auth): add more cases

--- tests/auth/auth_test.py
+++ tests/auth/auth_test.py
@@ -20,6 +20,30 @@ def test_auth_register_user(client, fake):
     assert data["bank_account"]["account_type"] == "itbank"
 
 
+def test_auth_register_user_existing_username(client, session, fake):
+    existing_user = User(
+        email=fake.email(),
+        username=fake.user_name(),
+        full_name=fake.name(),
+        disabled=False,
+        hashed_password=get_password_hash(fake.password()),
+    )
+
+    session.add(existing_user)
+    session.commit()
+
+    new_user = {
+        "username": existing_user.username,
+        "password": fake.password(),
+    }
+
+    response = client.post(AUTH_REGISTER_COMPLETE_ENDPOINT, json=new_user)
+
+    assert response.status_code == 400
+    data = response.json()
+    assert data["detail"] == "Username already registered"
+
+
 def test_auth_get_token(client, session, fake):
     login_data = {
         "username": fake.user_name(),
@@ -42,3 +66,15 @@ def test_auth_get_token(client, session, fake):
     data = response.json()
     assert "access_token" in data
     assert data["token_type"] == "bearer"
+
+
+def test_auth_get_token_invalid_credentials(client, session, fake):
+    login_data = {
+        "username": fake.user_name(),
+        "password": fake.password(),
+    }
+
+    response = client.post(AUTH_GET_TOKEN_COMPLETE_ENDPOINT, data=login_data)
+    assert response.status_code == 401
+    data = response.json()
+    assert data["detail"] == "Incorrect username or password"
